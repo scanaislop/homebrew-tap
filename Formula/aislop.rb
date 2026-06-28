87c7ff8651110c5fe0ae83abbb31a3192cb9b6f7
Kehinde Olawuwo <olawuwokenny@gmail.com>
2026-06-29 00:43:29 +0100

    fix: correct aislop 0.13.0 npm tarball sha256

--- Formula/aislop.rb
+++ Formula/aislop.rb
@@ -2,7 +2,7 @@ class Aislop < Formula
   desc "Scan your code for AI slop"
   homepage "https://scanaislop.com"
   url "https://registry.npmjs.org/aislop/-/aislop-0.13.0.tgz"
-  sha256 "c3982cbaecd98f6f0c63e95561e18eb66beb62b197b1aac28990651b1ce230bd"
+  sha256 "316bc4c45c6a04227a5c24f5e0bea336e80d336f158c36116a3bd3768d5423e5"
   license "MIT"
 
   depends_on "node"
