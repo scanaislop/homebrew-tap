ef5ea8bc1bd4bac9f3d3d26ff54d71cf0e9066ea
Kehinde Olawuwo <olawuwokenny@gmail.com>
2026-07-23 23:59:37 +0100

    fix(release): reset formula revision on bump

--- scripts/update-formula.sh
+++ scripts/update-formula.sh
@@ -8,5 +8,6 @@ sha256="$(curl -fsSL "$url" | shasum -a 256 | awk '{print $1}')"
 
 perl -0pi -e "s#url \"https://registry\\.npmjs\\.org/aislop/-/aislop-[^\"]+\\.tgz\"#url \"$url\"#" "$formula"
 perl -0pi -e "s#sha256 \"[a-f0-9]+\"#sha256 \"$sha256\"#" "$formula"
+perl -0pi -e 's/^\s*revision\s+\d+\n//m' "$formula"
 
 echo "Updated $formula to aislop $version ($sha256)"
