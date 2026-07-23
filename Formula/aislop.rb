715d4adc1d5f1a434a7be3c55368650020bd92de
Kehinde Olawuwo <olawuwokenny@gmail.com>
2026-07-23 23:48:28 +0100

    fix(formula): preserve bundled lint coverage

--- Formula/aislop.rb
+++ Formula/aislop.rb
@@ -4,8 +4,11 @@ class Aislop < Formula
   url "https://registry.npmjs.org/aislop/-/aislop-0.14.0.tgz"
   sha256 "fafa6de815b3588d8d7f34d5b148a5a06bc3954bf52bac0addd515872c014ab1"
   license "MIT"
+  revision 1
 
+  depends_on "golangci-lint"
   depends_on "node"
+  depends_on "ruff"
 
   def install
     system "npm", "install", *std_npm_args(ignore_scripts: false)
@@ -15,5 +18,7 @@ class Aislop < Formula
   test do
     assert_match version.to_s, shell_output("#{bin}/aislop --version")
     assert_match "aislop scan", shell_output("#{bin}/aislop --help")
+    assert_predicate formula_opt_bin("golangci-lint")/"golangci-lint", :executable?
+    assert_predicate formula_opt_bin("ruff")/"ruff", :executable?
   end
 end
